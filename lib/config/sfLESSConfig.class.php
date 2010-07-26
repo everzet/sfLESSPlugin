888ba4c527ecceb6fb3a438dac3d443234f6b077
ever.zet <ever.zet@gmail.com>
2010-07-26 18:41:01 +0300

    fixed double declaration

--- lib/config/sfLESSConfig.class.php
+++ lib/config/sfLESSConfig.class.php
@@ -55,15 +55,7 @@ class sfLESSConfig extends LESSConfig
    */
   public function getLessJsPath()
   {
-    return sfConfig::get('app_sf_less_plugin_js', '/sfLESSPlugin/js/less-1.0.33.min.js');
-  }
-
-  /**
-   * @see LESSConfig
-   */
-  public function getLessJsPath()
-  {
-    return sfConfig::get('app_sf_less_plugin_js_lib', parent::getLessJsPath());
+    return sfConfig::get('app_sf_less_plugin_js_lib', '/sfLESSPlugin/js/less-1.0.33.min.js');
   }
 
   /**
