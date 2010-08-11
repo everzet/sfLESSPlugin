d5cd8bbf7b205e22a8ce1e1d55c6e2011cd7faba
Victor Berchet <victor.berchet@yahoo.com>
2010-08-11 19:03:04 +0800

    Fixing unit test bootstrap path

--- test/unit/dependencyTest.php
+++ test/unit/dependencyTest.php
@@ -16,7 +16,7 @@
  * @version    1.0.0
  */
 
-include dirname(__FILE__).'/../../../../../test/bootstrap/unit.php';
+include dirname(__FILE__).'/../../../../test/bootstrap/unit.php';
 
 $t = new lime_test(8);
 
